8b6bf4db31d8363845057b841026e834e673b1d2
deivananderson <deyvananderson@hotmail.com>
2017-04-29 00:54:53 -0500

    account client OK

--- EsteticApp/app/src/main/java/cosw/eci/edu/esteticapp/activities/AccountClientActivity.java
+++ EsteticApp/app/src/main/java/cosw/eci/edu/esteticapp/activities/AccountClientActivity.java
@@ -1,11 +1,14 @@
 package cosw.eci.edu.esteticapp.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import cosw.eci.edu.esteticapp.R;
@@ -20,6 +23,7 @@ public class AccountClientActivity extends AppCompatActivity {
     private String nameR;
     private String emailR;
     private String passwordR;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,8 @@ public class AccountClientActivity extends AppCompatActivity {
         email = (EditText)findViewById(R.id.email);
         password = (EditText)findViewById(R.id.password);
         name = (EditText)findViewById(R.id.name);
+        save = (Button)findViewById(R.id.save);
+        save.setVisibility(Button.INVISIBLE);
         modified = 0;
         SharedPreferences mPrefs = getSharedPreferences("esteticapp.login.credential",123);
         String emailR = mPrefs.getString("email", "");
@@ -47,11 +53,13 @@ public class AccountClientActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(modified==0){
+                    save.setVisibility(Button.VISIBLE);
                     email.setEnabled(true);
                     password.setEnabled(true);
                     name.setEnabled(true);
                     modified =1;
                 }else {
+                    save.setVisibility(Button.INVISIBLE);
                     email.setEnabled(false);
                     password.setEnabled(false);
                     name.setEnabled(false);
@@ -61,4 +69,47 @@ public class AccountClientActivity extends AppCompatActivity {
         });
     }
 
+
+    public void saveData(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Data update")
+                .setMessage("Secure data update?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        email = (EditText)findViewById(R.id.email);
+                        password = (EditText)findViewById(R.id.password);
+                        name = (EditText)findViewById(R.id.name);
+                        SharedPreferences mPrefs = getSharedPreferences("esteticapp.login.credential", 123);
+                        SharedPreferences.Editor editor = mPrefs.edit();
+                        editor.putString("name", name.getText().toString());
+                        editor.putString("email", email.getText().toString());
+                        editor.putString("password", password.getText().toString());
+                        editor.putString("rol", "client");
+                        editor.commit();
+                        save.setVisibility(Button.INVISIBLE);
+                        email.setEnabled(false);
+                        password.setEnabled(false);
+                        name.setEnabled(false);
+                        modified =0;
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences mPrefs = getSharedPreferences("esteticapp.login.credential",123);
+                        String emailR = mPrefs.getString("email", "");
+                        String nameR = mPrefs.getString("name", "");
+                        String passwordR = mPrefs.getString("password", "");
+                        name.setText(nameR);
+                        email.setText(emailR);
+                        password.setText(passwordR);
+                        save.setVisibility(Button.INVISIBLE);
+                        email.setEnabled(false);
+                        password.setEnabled(false);
+                        name.setEnabled(false);
+                        modified =0;
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 }
